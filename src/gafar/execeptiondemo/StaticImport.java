7ad30b746ca8f9a204e4100bcf813dac3a1d71ca
samad <smohammadgafar@gmail.com>
2018-03-22 19:43:14 +0530

    static import memeber

---
+++ src/gafar/execeptiondemo/StaticImport.java
@@ -0,0 +1,9 @@
+package gafar.execeptiondemo;
+
+public class StaticImport {
+	public static final int INCREMENT=2;
+	public static int incrementNumber(int number){
+		return number+INCREMENT;
+	}
+
+}
