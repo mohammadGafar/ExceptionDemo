283c2f5bed81df8083061c832434562d9930e681
samad <smohammadgafar@gmail.com>
2018-03-22 19:41:51 +0530

    static import

---
+++ src/gafar/com/StaticImportDemo.java
@@ -0,0 +1,13 @@
+package gafar.com;
+
+import static gafar.execeptiondemo.StaticImport.*;
+public class StaticImportDemo {
+
+	public static void main(String[] args) {
+		System.out.println("increment"+INCREMENT);
+		int count=10;
+		System.out.println("increment count"+incrementNumber(count));
+
+	}
+
+}
