1566782b728ed3f309a62cd8a3430db7ac41db3f
samad <smohammadgafar@gmail.com>
2018-03-23 20:00:13 +0530

    arraysort

---
+++ src/gafar/com/ArraySort.java
@@ -0,0 +1,15 @@
+package gafar.com;
+
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		int a[]={1,3,6,33,45,65,78};
+		Arrays.sort(a);
+		for(int num:a){
+			System.out.println(num);
+		}
+	}
+
+}
