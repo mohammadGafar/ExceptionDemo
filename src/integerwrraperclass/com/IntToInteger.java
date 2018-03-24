90916c33070ee5e8ea249698c8f1f849b74fabf5
samad <smohammadgafar@gmail.com>
2018-03-24 18:21:41 +0530

    int to integer wrapper class

---
+++ src/integerwrraperclass/com/IntToInteger.java
@@ -0,0 +1,15 @@
+package integerwrraperclass.com;
+
+public class IntToInteger {
+	
+	public static void main(String [] args){
+		
+		int i=10;
+		Integer intg=new Integer(i);
+		System.out.println(intg);
+		String no="222";
+		Integer num=new Integer(no);
+		System.out.println(num);
+	}
+
+}
