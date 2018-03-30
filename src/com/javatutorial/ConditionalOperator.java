784b118985a55a3ca6a0702ccd43d7d525737d01
samad <smohammadgafar@gmail.com>
2018-03-30 18:40:05 +0530

    conditional operator

---
+++ src/com/javatutorial/ConditionalOperator.java
@@ -0,0 +1,14 @@
+package com.javatutorial;
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=10;
+		b=(a==1)?10:20;
+		System.out.println(b);
+		b=(a==10)?20:20;
+		System.out.println(b);
+	}
+
+}
