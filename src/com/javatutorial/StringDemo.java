056ff5593862444b59df701a754e04a0ee1c475c
samad <smohammadgafar@gmail.com>
2018-03-31 19:56:42 +0530

    StringDemo

---
+++ src/com/javatutorial/StringDemo.java
@@ -0,0 +1,13 @@
+package com.javatutorial;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		char Array[]={'h','e','l','l','o'};
+		String str= new String(Array);
+		System.out.println(str);
+		String country="India";
+		System.out.println(country);
+	}
+
+}
