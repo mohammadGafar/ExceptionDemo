3fe32237188a2420d6235cd69b7238efcaf2092a
samad <smohammadgafar@gmail.com>
2018-04-18 19:29:01 +0530

    String CharAt

---
+++ src/com/javatutorial/SChatAt.java
@@ -0,0 +1,11 @@
+package com.javatutorial;
+
+public class SChatAt {
+
+	public static void main(String[] args) {
+		String s = "string are immutable";
+		char result = s.charAt(7);
+		System.out.println(result);
+	}
+
+}
