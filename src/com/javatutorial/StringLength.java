71b62260eb68cbc8fb3c5309812097cf39708cc5
samad <smohammadgafar@gmail.com>
2018-04-18 19:22:48 +0530

    string length

---
+++ src/com/javatutorial/StringLength.java
@@ -0,0 +1,19 @@
+package com.javatutorial;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String palindrome="what is your name";
+		int len=palindrome.length();
+		System.out.println("string length"+len);
+		String name="you";
+		System.out.println("hmmm" + name + "tell me");
+		
+		//formating string
+		
+		String fs;
+		fs=String.format("the vale" + "of the" + "float variable is" + "here are");
+		System.out.println(fs);
+	}
+
+}
