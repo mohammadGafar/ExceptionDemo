524b3c137ece0d0baa550af0ed383f1077432f56
samad <smohammadgafar@gmail.com>
2018-04-23 19:34:40 +0530

    overridden

---
+++ src/com/javatutorial/Commandlinedemo.java
@@ -0,0 +1,11 @@
+package com.javatutorial;
+
+public class Commandlinedemo {
+
+	public static void main(String[] args) {
+		for(int i=0;i<args.length;i++){
+			System.out.println("args["+i+"]:"+args[i]);
+		}
+	}
+
+}
