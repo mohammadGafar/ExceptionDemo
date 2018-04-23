e28c922276bc6e09e04438bed37d7053553f5af4
samad <smohammadgafar@gmail.com>
2018-04-23 18:50:02 +0530

    is a relationship demo

---
+++ src/com/javatutorial/Dog.java
@@ -0,0 +1,24 @@
+package com.javatutorial;
+
+class Animal{
+	
+}
+class Mammal extends Animal{
+	
+}
+class Reptail extends Mammal{
+	
+}
+public class Dog extends Mammal {
+
+	public static void main(String[] args) {
+		Animal a=new Animal();
+		Mammal m =new Mammal();
+		Dog d=new Dog();
+		
+		System.out.println(m instanceof Animal);
+		System.out.println(d instanceof Mammal);
+		System.out.println(d  instanceof Animal);
+	}
+
+}
