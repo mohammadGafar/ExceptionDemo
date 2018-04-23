8d4842a1f73bb56deb29ec45d473db1c450ebc14
samad <smohammadgafar@gmail.com>
2018-04-23 18:09:20 +0530

    calculate class

---
+++ src/com/javatutorial/Calculate.java
@@ -0,0 +1,13 @@
+package com.javatutorial;
+
+public class Calculate {
+	int z;
+	public void addition(int x,int y){
+		z=x+y;
+		System.out.println("the sum of given numbers is :"+z);
+	}
+	public void substraction(int x,int y){
+		z=x-y;
+		System.out.println("substraction of given numbers is :"+z);
+	}
+}
