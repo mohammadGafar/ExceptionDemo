54a15799147ad16e890e16d5f3a7be088a4233cf
samad <smohammadgafar@gmail.com>
2018-04-25 17:22:56 +0530

    InnerDemo

---
+++ src/com/javatutorial/InnerClassDemo.java
@@ -0,0 +1,21 @@
+package com.javatutorial;
+
+class Outer_Demo{
+	int num;
+	private class Inner_Demo{
+		public void print(){
+			System.out.println("this is an inner class");
+		}
+	}
+	void display_Inner(){
+		Inner_Demo inner=new Inner_Demo();
+		inner.print();
+	}
+}
+public class InnerClassDemo {
+	public static void main(String[]args){
+		Outer_Demo outer=new Outer_Demo();
+		outer.display_Inner();
+	}
+
+}
