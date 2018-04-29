1999565682d657cb3ab72c03616cf8c71a767629
samad <smohammadgafar@gmail.com>
2018-04-29 18:45:21 +0530

    Collection Demo

---
+++ src/com/advancedjavatutorial/CollectionDemo.java
@@ -0,0 +1,19 @@
+package com.advancedjavatutorial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		Map m = new HashMap();
+		m.put("zara", 8);
+		m.put("mahnaz",7);
+		m.put("ayan",31);
+		m.put("daisy",24);
+		System.out.println();
+		System.out.println("map elements");
+		System.out.println("\t"+m);
+	}
+
+}
