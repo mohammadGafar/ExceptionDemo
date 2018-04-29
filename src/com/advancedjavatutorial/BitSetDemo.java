63808e63b0ee0214d0cda2ea6396833b414b4176
samad <smohammadgafar@gmail.com>
2018-04-29 15:03:45 +0530

    BitSetDemo

---
+++ src/com/advancedjavatutorial/BitSetDemo.java
@@ -0,0 +1,33 @@
+package com.advancedjavatutorial;
+
+import java.util.BitSet;
+
+public class BitSetDemo {
+
+	public static void main(String[] args) {
+		BitSet bits1 = new BitSet(16);
+		BitSet bits2 = new BitSet(16);
+		for(int i=0;i<16;i++){
+			if((i%2) == 0) bits1.set( i);
+			if((i%5) !=0) bits2.set(i);
+		}
+		System.err.println("initial pattern in bits1");
+		System.out.println(bits1);
+		
+		System.out.println("initial pattern in bits2");
+		System.out.println(bits2);
+		
+		bits2.and(bits1);
+		System.out.println("\n bits2 And bits1");
+		System.out.println(bits2);
+		
+		bits2.or(bits1);
+		System.out.println("\n bits2 or bits1");
+		System.out.println(bits2);
+		
+		bits2.xor(bits1);
+		System.out.println("\n bits2 xor bits1");
+		System.out.println(bits2);
+	}
+
+}
